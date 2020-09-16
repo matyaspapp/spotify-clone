1f27cea3a3bf01bb0ce1c18df06035d31d9591be
matyaspapp <project.matyas.papp@protonmail.com>
2020-09-16 14:17:09 +0200

    add spotify login and get token from it

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@
 */
 import React from 'react';
 import './App.css';
+import Login from './Components/Login';
 
 function App() {
   return (
     <div className="app">
-      
+      <Login />
     </div>
   );
 }
---
+++ src/spotify.js
@@ -0,0 +1,14 @@
+const authEndpoint = 'https://accounts.spotify.com/authorize';
+const redirectUri = 'http://localhost:3000/';
+const clientId = '3353e643f35b495a925019c575c75a9a';
+
+const scopes = [
+  'user-read-currently-playing',
+  'user-read-recently-played',
+  'user-read-playback-state',
+  'user-top-read',
+  'user-modify-playback-state',
+];
+
+export const loginUrl =
+  `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
