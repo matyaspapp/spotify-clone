373353a7e7f7b8c2cca9572d06a0596ca75e2db7
matyaspapp <project.matyas.papp@protonmail.com>
2020-09-16 14:46:22 +0200

    add token parser

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@
  * use BEM naming convention
  * https://en.bem.info/methodology/naming-convention/
 */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Components/Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = '';
+
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+    }
+  }, []);
+
   return (
     <div className="app">
-      <Login />
+      {
+        token ? (
+          <h1>You are logged in!</h1>
+        ) : (
+          <Login />
+        )
+      }
     </div>
   );
 }
--- src/spotify.js
+++ src/spotify.js
@@ -12,3 +12,13 @@ const scopes = [
 
 export const loginUrl =
   `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
+
+export const getTokenFromUrl = () =>
+  window.location.hash
+    .substring(1)
+    .split('&')
+    .reduce((initial, item) => {
+      const parts = item.split('=');
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+      return initial
+    }, {})
