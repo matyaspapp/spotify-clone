21d30ee6bc82832a14a5ec28123d5fa198a43eee
matyaspapp <project.matyas.papp@protonmail.com>
2020-09-17 17:11:54 +0200

    add more style to sidebar option

--- src/Components/PlayerSidebar/index.js
+++ src/Components/PlayerSidebar/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './index.css';
+import PlayerSidebarOption from '../PlayerSidebarOption';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
 
 const PlayerSidebar = () => {
   return (
@@ -9,6 +13,17 @@ const PlayerSidebar = () => {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
       />
+      <PlayerSidebarOption title="Home" Icon={HomeIcon}/>
+      <PlayerSidebarOption title="Searc" Icon={SearchIcon}/>
+      <PlayerSidebarOption title="Your Library" Icon={LibraryMusicIcon}/>
+
+      <br />
+      <strong className="playerSidebar__title">PLAYLISTS</strong>
+      <hr />
+
+      <PlayerSidebarOption title="Hip hop" />
+      <PlayerSidebarOption title="R'n'B" />
+      <PlayerSidebarOption title="Rock" />
     </div>
   );
 }
--- src/Components/PlayerSidebarOption/index.js
+++ src/Components/PlayerSidebarOption/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './index.css';
 
-const PlayerSidebarOption = () => {
+const PlayerSidebarOption = ({ title, Icon }) => {
   return (
     <div className="playerSidebarOption">
-      
+      {Icon && <Icon className="playerSidebarOption__icon" />}
+      {Icon ? <h4>{title}</h4> : <p>{title}</p>}
     </div>
   );
 }
