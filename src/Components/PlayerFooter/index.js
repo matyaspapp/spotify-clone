380122ea36d86b253eef33fd90b0a4250f650a03
matyaspapp <project.matyas.papp@protonmail.com>
2020-09-17 15:31:15 +0200

    add player footer component

---
+++ src/Components/PlayerFooter/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+const PlayerFooter = () => {
+  return (
+    <div className="playerFooter">
+      
+    </div>
+  );
+}
+
+export default PlayerFooter;
