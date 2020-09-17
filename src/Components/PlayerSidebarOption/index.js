01f3768fc5118fafeb0e99e22feca01beb81bd4f
matyaspapp <project.matyas.papp@protonmail.com>
2020-09-17 16:37:34 +0200

    add sidebar option component

---
+++ src/Components/PlayerSidebarOption/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+const PlayerSidebarOption = () => {
+  return (
+    <div className="playerSidebarOption">
+      
+    </div>
+  );
+}
+
+export default PlayerSidebarOption;
